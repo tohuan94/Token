f2efa284ba89fc3ef88e2942f2947029a44333d0
tohuan94 <80341288+tohuan94@users.noreply.github.com>
2025-07-03 20:03:01 +0700

    Create Token.sol

---
+++ Token.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Token {
+    string public name = "MyToken";
+    string public symbol = "MTK";
+    uint256 public totalSupply = 1000000;
+    mapping(address => uint256) public balanceOf;
+
+    constructor() {
+        balanceOf[msg.sender] = totalSupply;
+    }
+}
